4574406f85d4e1f834f322296ddd7d3dd6c93509
khhandrea <khhandrea@naver.com>
2024-05-15 16:57:36 +0900

    feat: Add visual cartpole

--- environments/__init__.py
+++ environments/__init__.py
@@ -4,5 +4,5 @@ register(id="pytorch-predictive-agent/LinearSpectrumEnvironment-v0",
          entry_point="environments.linear_spectrum_environment:LinearSpectrumEnvironment")
 register(id="pytorch-predictive-agent/MovingImageEnvironment-v0",
          entry_point="environments.moving_image_environment:MovingImageEnvironment")
-register(id="pytorch-predictive-agent/VisualCartpole-v0",
-         entry_point="environments.visual_cartpole:VisualCartpole")
+register(id="pytorch-predictive-agent/VisualCartPole-v0",
+         entry_point="environments.visual_cartpole:VisualCartPole")
